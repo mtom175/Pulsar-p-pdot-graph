657fcbb7030893710d1ffd19e5803437e7c8f25e
Morgan Thomas <morgantom175@yahoo.com>
2026-09-08 00:46:27 +0100

    Add files via upload
    After some time I realised that I probably do not need to do for loops, and rather just implement the data. I found NGC104 pulsar data from the following website "https://www3.mpifr-bonn.mpg.de/staff/pfreire/GCpsr.txt" which I used to plot a P/Pdot graph. I am unsure if what I have plotted is on the right lines or not. However it is progress. I do want to try and derive my own periods for the pulsars within NGC104 using the period data, and I think it would be interesting to see if I can use for loops to evolve the pulsars over time. This could yield some interesting results, like predicting the spin down of the pulsars.

--- P_Pdot.py
+++ P_Pdot.py
@@ -7,36 +7,40 @@ import numpy as np
 import math
 
 #Pulsars within NGC104
-pulsar = pd.read_csv['']
+pulsar = pd.read_csv("Data from https://www3.mpifr-bonn.mpg.de/staff/pfreire/GCpsr.txt")
 #pulsar paramters here
 
-P = [] #Pulsar's period'
-T = [] #time of the period
+P = pulsar["PERIOD_(ms)"] #Pulsar's period'
+print(P, "as period of pulsars")
+Pdot = pulsar["Pdot_(10-20)"] #dervitive of the pulsar
+print(Pdot, "as dervitive of the period of pulsars")
+PdotLog = []
+#T = [] #time of the period
 
-pf = [] #final period
-po = [] #start peiord
-tf = [] #final time
-to = [] #start time
+#pf = [] #final period
+#po = [] #start peiord
+#tf = [] #final time
+#to = [] #start time
 
 #I may have to do a for loop for each period found with respect to time. something like; for i in time: period(i)... possibly something like that. As i think with my current set up it only looks at a single pulsar periods and #time related not multiple.
 
-def period(p, t):
-    return((pf-po)/(tf-to))
+#def period(p, t):
+#    return((pf-po)/(tf-to))
 
 
-Pdot = period(P, T)  #Dervitibe of the Pulsar's period'
+#Pdot = period(P, T)  #Dervitibe of the Pulsar's period'
 
-print("Pdot") #Works for dummy veribles, so it should work for actual data.
+#print("Pdot") #Works for dummy veribles, so it should work for actual data.
 
-PdotLOG = math.log10('Pdot')
+#PdotLOG = math.log10('Pdot')
 
 #distance to NGC104. This will be used when plotting the line of best fit.
 d_pc = 4450
 mu = 5*np.log10(d_pc/10) # d_pc = cluster distance in parsecs
 
 #Age lines
-Ages_to_plot = [] #Should use the ages of the GC
-Ages_labels = [] #labels for ages
+#Ages_to_plot = [] #Should use the ages of the GC
+#Ages_labels = [] #labels for ages
 
 #radio band
 
@@ -46,5 +50,8 @@ Ages_labels = [] #labels for ages
 
 #plotting
 plt.figure(figsize = (12, 12))
-plt.scatter(P, PdotLOG, marker = '*', s=8, label="NGC104_pulsars", color = "black")
-
+plt.scatter(P, Pdot, marker = '*', s=5, label="NGC104_pulsars", color = "black")
+plt.xlabel("Pdot(10-20)", fontsize = 25)
+plt.ylabel("Period(ms)", fontsize = 25)
+plt.legend(fontsize = 25)
+plt.show()
